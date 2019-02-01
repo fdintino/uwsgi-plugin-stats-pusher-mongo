223809988036c7c56797842e79a316d138a5c904
Frankie Dintino <fdintino@gmail.com>
2019-01-31 23:40:43 -0500

    Actually address the worker index issues

--- transform_metrics.cc
+++ transform_metrics.cc
@@ -11,7 +11,7 @@ using json = nlohmann::json;
  *
  * The full stats JSON object contains structures that conveniently map onto
  * the metric keys. For instance, using the example above
- * (worker.0.core.0.requests), we find in the full stats object:
+ * (worker.1.core.0.requests), we find in the full stats object:
  *
  *     {
  *         "workers": [{
@@ -22,6 +22,11 @@ using json = nlohmann::json;
  * For metric key components that precede an array index, these must be
  * pluralized (worker, core, and socket become workers, cores, and sockets,
  * respectively).
+ *
+ * Also note that, in the above example of worker.1.core, the worker id is
+ * one-indexed, not zero-indexed. This is because the overall stats apply to
+ * the master worker. Thus worker.0.avg_response_time actually corresponds
+ * to the JSON path /avg_response_time.
  *
  * [1] In mongo-c-driver >= 1.6.0, documents with keys containing '.' are not
  *     permitted, since the resulting document is almost impossible to query:
@@ -38,16 +43,37 @@ static std::string metrics_key_to_json_pointer_path(std::string key) {
     std::string::size_type last = 0;
     std::string::size_type pos, next;
 
+    std::string component;
+
     while (std::string::npos != (pos = key.find(".", last))) {
-        // Append next path component
-        path.append(key, last, pos - last);
+        component = key.substr(last, pos - last);
 
         bool is_array_key = (pos < max && std::isdigit(key.at(pos + 1)));
 
         if (is_array_key) {
-            path += "s";
+            component += "s";
+            if (component == "workers") {
+                // worker index fix
+                if (std::string::npos != (next = key.find(".", pos + 1))) {
+                    int worker_num = stoi(key.substr(pos + 1, next), nullptr, 10);
+                    // If worker == 0, don't append workers/0/ (since the stat
+                    // applies to the master worker), otherwise subtract 1 from
+                    // the worker id to make it zero-indexed
+                    if (worker_num == 0) {
+                        // append nothing, continue past the worker id
+                        last = next + 1;
+                        continue;
+                    } else {
+                        component += "/" + std::to_string(worker_num - 1);
+                        // Advance 'pos' past the worker id, so we don't
+                        // append it twice
+                        pos = next;
+                    }
+                }
+            }
         }
-        path += "/";
+
+        path += component + "/";
         last = pos + 1;
     }
     // Add remaining key after the last "."
